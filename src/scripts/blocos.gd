ce85a66c2cb396e2c2a4ae0e6cfc3824cc3acd01
Erick Augusto <erickaugusto1234@gmail.com>
2025-09-18 08:38:20 -0300

    Sistema de mudança de posição de blocos adiconado

--- src/scripts/blocos.gd
+++ src/scripts/blocos.gd
@@ -7,16 +7,10 @@ signal mover(dir)
 # Assets a serem carregados na memória
 var bloco_comando = load("res://scenes/bloco_comando.tscn")
 
+var sobre_bloco = null
 
 # Número de blocos de código por linha
 @export var max_blocos_linha = 5
-
-# Posição do próximo comando no visor da lista
-var linha = 0
-var coluna = 0
-
-# Lista de comandos a serem executados em ordem
-var comandos = []
 
 # O modo do mouse define se o jogador está segurando um bloco ou não.
 var modo = false
@@ -49,19 +43,40 @@ func add_elemento(acao):
 		modo = true
 
 # Remove um comando da lista
-func rm_elemento():
-	if get_tree().get_node_count_in_group("bloco_comando") != 0:
-		get_tree().get_nodes_in_group("bloco_comando")[-1].queue_free()
+func rm_elemento(pos_fila):
+	if get_tree().get_node_count_in_group("bloco_comando") > 0:
+		if pos_fila < 0:
+			pos_fila = get_tree().get_node_count_in_group("bloco_comando")
 		
-		if coluna == 0 and linha >= 1:
-			coluna = 4
-			linha -= 1
+		print("Removendo o nó na posição: ", pos_fila)
 		
-		elif coluna >= 1:
-			coluna -= 1
-	
-	comandos.pop_back()
+		for cmd in get_tree().get_nodes_in_group("bloco_comando"):
+			if cmd.pos_fila == pos_fila:
+				cmd.queue_free()
+			
+			if cmd.pos_fila > pos_fila:
+				cmd.pos_fila -= 1
+				
+				posicionar_elemento(cmd)
 
+# Posiciona um bloco de comando na visualização
+func posicionar_elemento(bloco):
+	var coluna = (bloco.pos_fila - 1) % 5
+	var linha = (bloco.pos_fila - 1) / 5
+	
+	bloco.position.x = 8 + coluna * 64
+	bloco.position.y = 8 + linha * 64
+	
+	print("linha: ", linha)
+	print("coluna: ", coluna)
+
+func reposicionar_elemento(bloco):
+	for cmd in get_tree().get_nodes_in_group("bloco_comando"):
+		if cmd.pos_fila >= bloco.pos_fila:
+			cmd.pos_fila += 1
+			posicionar_elemento(cmd)
+	
+	
 
 # Função principal
 func _process(_delta: float) -> void:
@@ -74,26 +89,30 @@ func _process(_delta: float) -> void:
 		# Se o mouse for solto
 		if Input.is_action_just_released("mouse_click"):
 			if modo and mouse_dentro:
+				var bloco = bloco_comando.instantiate()
+				$InputList.add_child(bloco)
+				bloco.clicado.connect(_on_clicado)
+				bloco.entrar.connect(_on_comando_entrar)
+				bloco.sair.connect(_on_comando_sair)
+				bloco.tipo = get_tree().get_nodes_in_group("bloco_temporario")[0].get_meta("tipo")
+				bloco.pos_fila = get_tree().get_node_count_in_group("bloco_comando")
 				
-				comandos.append(get_tree().get_nodes_in_group("bloco_temporario")[0].get_meta("tipo"))
+				posicionar_elemento(bloco)
+			
+			elif modo and sobre_bloco != null:
+				var pos = sobre_bloco.pos_fila
+				
+				reposicionar_elemento(sobre_bloco)
 				
 				var bloco = bloco_comando.instantiate()
 				$InputList.add_child(bloco)
 				bloco.clicado.connect(_on_clicado)
-				bloco.tipo = comandos[-1]
+				bloco.entrar.connect(_on_comando_entrar)
+				bloco.sair.connect(_on_comando_sair)
+				bloco.tipo = get_tree().get_nodes_in_group("bloco_temporario")[0].get_meta("tipo")
+				bloco.pos_fila = pos
 				
-				bloco.position.x = 8 + coluna * 64
-				bloco.position.y = 8 + linha * 64
-				
-				if coluna == max_blocos_linha - 1:
-					linha += 1
-					coluna = 0
-				
-				else:
-					coluna += 1
-				
-				print("linha: ", linha)
-				print("coluna: ", coluna)
+				posicionar_elemento(bloco)
 			
 			get_tree().call_group("bloco_temporario", "queue_free")
 			modo = false
@@ -116,12 +135,16 @@ func _on_baixo_button_down() -> void:
 func _on_executar_pressed() -> void:
 	executar.emit()
 	
-	for cmd in comandos:
-		mover.emit(cmd)
+	var lista = get_tree().get_nodes_in_group("bloco_comando")
+	lista.sort_custom(func(a, b): return a.pos_fila < b.pos_fila)
+	
+	for cmd in lista:
+		mover.emit(cmd.tipo)
 		# print(cmd)
 
+
 func _on_remover_pressed() -> void:
-	rm_elemento()
+	rm_elemento(-1)
 
 
 # Os métodos e atributos abaixo verificam se o mouse está na área correta
@@ -139,4 +162,12 @@ func _on_clicado(caminho) -> void:
 	if not modo:
 		var bloco = get_node(caminho)
 		add_elemento(bloco.tipo)
-		rm_elemento()
+		rm_elemento(bloco.pos_fila)
+
+
+# Verifica se o mouse está sobre um bloco de comando.
+func _on_comando_entrar(path) -> void:
+	sobre_bloco = get_node(path)
+
+func _on_comando_sair() -> void:
+	sobre_bloco = null
--- src/scripts/comando.gd
+++ src/scripts/comando.gd
@@ -2,7 +2,11 @@ extends StaticBody2D
 
 signal clicado(path)
 
+signal entrar(path)
+signal sair()
+
 @export var tipo = "r"
+@export var pos_fila = 0
 
 var dentro
 
@@ -26,8 +30,10 @@ func _process(_delta: float) -> void:
 
 func _on_color_rect_mouse_entered() -> void:
 	dentro = true
+	entrar.emit(get_path())
 	$BG.color = Color(1.0, 1.0, 1.0, 0.125)
 
 func _on_color_rect_mouse_exited() -> void:
 	dentro = false
+	sair.emit()
 	$BG.color = Color(1.0, 1.0, 1.0, 0.0)
